7f2de638ebcc30ab80e696f2b38fdcb4d0ce39e3
sebastianleon6 <sebastianleon219@gmail.com>
2025-11-26 20:34:21 -0500

    subiendo al repo

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        boolean accesoExclisivo = true;
+        boolean calificaPromocion = true;
+
+
+
+
+    }
+}
